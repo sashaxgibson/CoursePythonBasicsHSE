fe791bd870b888d8d036b0bd712e3679f0ae389c
sashaxgibson <2237336+sashaxgibson@users.noreply.github.com>
2021-04-25 06:50:05 +0300

    Update W6_SortParticipants.py

--- W6_SortParticipants.py
+++ W6_SortParticipants.py
@@ -1,3 +1,7 @@
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
-lines = inFile.readlines()
+nameList = inFile.readlines()
+print(nameList)
+a = nameList[0]
+print(a)
+print(a.strip())
