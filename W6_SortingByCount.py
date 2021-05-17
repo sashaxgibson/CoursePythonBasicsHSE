04200d4567fe1f6261bf6865ec4f1f6347552003
sashaxgibson <2237336+sashaxgibson@users.noreply.github.com>
2021-05-17 14:28:33 +0300

    Update W6_SortingByCount.py

--- W6_SortingByCount.py
+++ W6_SortingByCount.py
@@ -1,3 +1,16 @@
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
-dataList = inFile.readlines()
+dataStr = inFile.readlines()
+dataList = []
+for i in range(len(dataStr)):
+    dataList.append(dataStr[i].split())
+print(dataList)
+
+def countSort(N):
+    funcList = []
+    for i in N:
+        funcList[i] += 1
+    print(funcList)
+
+
+countSort(dataList)
