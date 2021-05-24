e405418c329fa6341ab55e44a13b9878c726300d
sashaxgibson <riseupgbs@gmail.com>
2021-05-24 09:26:47 +0300

    Update W6_OlimpiaResults.py
    решение

--- W6_OlimpiaResults.py
+++ W6_OlimpiaResults.py
@@ -1,7 +1,9 @@
 N = int(input())
 dataList = []
-while N != 0:
-    a = input().split()
-    dataList.append(a)
-    N -= 1
-print(dataList)
+for i in range(N):
+    tempManData = input().split()
+    manData = (tempManData[0], int(tempManData[1]))
+    dataList.append(manData)
+dataList.sort(key=lambda x: x[1], reverse=True)
+for i in range(len(dataList)):
+    print(dataList[i][0])
