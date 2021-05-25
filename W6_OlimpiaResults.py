b9027ec582005dca801fd49ec23b7002f5f539cc
sashaxgibson <riseupgbs@gmail.com>
2021-05-25 11:51:14 +0300

    Update W6_OlimpiaResults.py

--- W6_OlimpiaResults.py
+++ W6_OlimpiaResults.py
@@ -7,3 +7,8 @@ for i in range(N):
 dataList.sort(key=lambda x: x[1], reverse=True)
 for i in range(len(dataList)):
     print(dataList[i][0])
+while N != 0:
+    a = input().split()
+    dataList.append(a)
+    N -= 1
+print(dataList)
