4b94e113c1fe93ea918c3187ef6fd7bdcada2a25
sashaxgibson <riseupgbs@gmail.com>
2021-05-29 15:11:59 +0300

    Update W6_PassingScore.py

--- W6_PassingScore.py
+++ W6_PassingScore.py
@@ -1,5 +1,7 @@
-K = int(input())
+#K = int(input())
+K = 5
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
 lines = inFile.readlines()
 print(lines)
+print(lines[0])
