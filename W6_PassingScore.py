74e5736bcbe04ba25d5d4560ccdab4b0437f5927
sashaxgibson <riseupgbs@gmail.com>
2021-05-31 01:49:38 +0300

    Update W6_PassingScore.py

--- W6_PassingScore.py
+++ W6_PassingScore.py
@@ -3,12 +3,21 @@ K = 5
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
 lines = inFile.readlines()
-print(lines)
 N = len(lines)
 myScoreList = []
 for now in lines:
     x = now.split()
-    tempMan = (int(x[-3]), int(x[-2]), int(x[-1]))
-    myScoreList.append(tempMan)
-print(myScoreList)
-print(sum(myScoreList[0]))
+    curManScore = (int(x[-3]), int(x[-2]), int(x[-1]))
+    myScoreList.append(curManScore)
+goodScoreList = []
+for now in myScoreList:
+    if now[0] and now[1] and now[2] >= 40:
+        goodScoreList.append(sum(now))
+    else:
+        continue
+goodScoreList.sort(reverse=True)
+if len(goodScoreList) > K:
+    answ = goodScoreList[4]
+    print(answ)
+elif len(goodScoreList) < K:
+        print(0)
