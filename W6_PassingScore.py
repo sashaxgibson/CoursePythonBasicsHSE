05cb3303f97e412a5a1736804d3ba99352ce1ecd
sashaxgibson <riseupgbs@gmail.com>
2021-05-30 17:37:26 +0300

    Update W6_PassingScore.py

--- W6_PassingScore.py
+++ W6_PassingScore.py
@@ -4,4 +4,11 @@ inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
 lines = inFile.readlines()
 print(lines)
-print(lines[0])
+N = len(lines)
+myScoreList = []
+for now in lines:
+    x = now.split()
+    tempMan = (int(x[-3]), int(x[-2]), int(x[-1]))
+    myScoreList.append(tempMan)
+print(myScoreList)
+print(sum(myScoreList[0]))
