a42ae2138b5fe16989b3d112a61b06442d263164
sashaxgibson <riseupgbs@gmail.com>
2021-06-03 06:38:04 +0300

    воркфлоу

--- W6_PassingScore.py
+++ W6_PassingScore.py
@@ -1,31 +1,25 @@
 inFile = open('input.txt', 'r', encoding='utf8')
 outFile = open('output.txt', 'w', encoding='utf8')
+K = int(inFile.readline())
 lines = inFile.readlines()
-K = int(lines[0])
-lines.pop(0)
 myScoreList = []
 for now in lines:
     x = now.split()
     curManScore = (int(x[-3]), int(x[-2]), int(x[-1]))
     myScoreList.append(curManScore)
-print(myScoreList)
 goodScoreList = []
 for now in myScoreList:
     if now[0] > 39 and now[1] > 39 and now[2] > 39:
-        print(now)
         goodScoreList.append(sum(now))
     else:
         continue
 goodScoreList.sort(reverse=True)
-print(goodScoreList)
 if len(goodScoreList) <= K:
         print(0, file=outFile)
 else:
-    if len(goodScoreList) > K:
-        passScore = goodScoreList[K - 1]
-        if passScore != goodScoreList[K]:
-            print(passScore, file=outFile)
-        else:
-            print(1, file=outFile)
+    if goodScoreList[0] == goodScoreList[K]:
+        print(1, file=outFile)
+    else:
+
 inFile.close()
 outFile.close()
