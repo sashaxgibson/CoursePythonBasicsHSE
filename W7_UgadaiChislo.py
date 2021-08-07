85a2a98bfc7cd55c4cafc3f11e6c5547f2b330e4
sashaxgibson <riseupgbs@gmail.com>
2021-08-07 03:53:08 +0300

    Update W7_UgadaiChislo.py

--- W7_UgadaiChislo.py
+++ W7_UgadaiChislo.py
@@ -1,4 +1,8 @@
 ##Ctrl + D чтобы остановить ввод в sys.stdin
 import sys
-a = list(sys.stdin.read().split())
+a = list(sys.stdin.readlines())
 print(a)
+b = int(a[0])
+print(b)
+check = "YES\n"
+print(check == a[2])
