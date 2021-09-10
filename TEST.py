c0fb2a11402286f5ad89fc05a5294fa01d7d49ce
sashaxgibson <riseupgbs@gmail.com>
2021-09-10 03:18:16 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,3 +1,9 @@
 #' '.join(s.split())
 a = 'benis \n'
 print(' '.join(a.split()))
+inFile = open('input.txt', 'r', encoding='utf8')
+outFile = open('output.txt', 'w', encoding='utf8')
+#nameList = inFile.readlines()
+#print(nameList)
+fuf = inFile.read()
+print(fuf)
