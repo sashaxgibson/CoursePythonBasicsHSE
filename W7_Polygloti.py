8da4ce2a28bbee9f51ed3b308db8f8627fe350e9
sashaxgibson <riseupgbs@gmail.com>
2021-09-21 12:52:56 +0300

    Update W7_Polygloti.py

--- W7_Polygloti.py
+++ W7_Polygloti.py
@@ -1,8 +1,9 @@
 inFile = open('input.txt', 'r', encoding='utf8')
+dataList = []
 dataList = inFile.readlines()
+print(dataList)
+clrList = list(map())
+print(clrList)
 N = int(dataList[0])
 allLang = set()
 comLang = set()
-i = 1
-while i <= N + 1:
-    curLangQ = int(dataList[i])
