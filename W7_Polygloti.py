b4c42eac8f51a1a8cb99eb2622340d1799cae042
sashaxgibson <riseupgbs@gmail.com>
2021-09-26 22:22:29 +0300

    Update W7_Polygloti.py

--- W7_Polygloti.py
+++ W7_Polygloti.py
@@ -3,6 +3,9 @@ dataList = []
 for line in inFile:
     dataList.append(line.strip())
 print(dataList)
-curQuanLang = int(dataList[1])
-tstList = dataList[2:2+int(dataList[1])]
+i = 1
+curQuanLang = int(dataList[i])
+tstList = dataList[i + 1:i + 1 + curQuanLang]
 print(tstList)
+i = 1
+while i < len(dataList):
