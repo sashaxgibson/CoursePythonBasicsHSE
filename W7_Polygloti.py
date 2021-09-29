cf4fa372eccbd850f88070ec564447b40480800c
sashaxgibson <riseupgbs@gmail.com>
2021-09-30 01:57:12 +0300

    task polygloti solved

--- W7_Polygloti.py
+++ W7_Polygloti.py
@@ -1,5 +1,23 @@
 inFile = open('input.txt', 'r', encoding='utf-8')
+N = int(inFile.readline())
 dataList = []
-for line in inFile:
-    dataList.append(line.strip())
-print(dataList)
+while N > 0:
+    langNum = int(inFile.readline())
+    tempList = []
+    while langNum > 0:
+        tempList.append(inFile.readline().strip())
+        langNum -= 1
+    dataList.append(tempList)
+    N -= 1
+comSet = set(dataList[0])
+for now in dataList:
+    comSet &= set(now)
+print(len(comSet))
+for x in comSet:
+    print(x)
+allSet = set()
+for x in dataList:
+    allSet |= set(x)
+print(len(allSet))
+for x in allSet:
+    print(x)
