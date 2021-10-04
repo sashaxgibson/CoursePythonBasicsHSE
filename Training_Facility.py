7a4b4563e7e83696a86d7a5a4d79d1c66af8ace5
sashaxgibson <riseupgbs@gmail.com>
2021-10-04 20:09:59 +0300

    Update Training_Facility.py

--- Training_Facility.py
+++ Training_Facility.py
@@ -1,23 +1,3 @@
-inFile = open('input.txt', 'r', encoding='utf-8')
-N = int(inFile.readline())
-dataList = []
-while N > 0:
-    langNum = int(inFile.readline())
-    tempList = []
-    while langNum > 0:
-        tempList.append(inFile.readline().strip())
-        langNum -= 1
-    dataList.append(tempList)
-    N -= 1
-comSet = set(dataList[0])
-for now in dataList:
-    comSet &= set(now)
-print(len(comSet))
-for x in comSet:
-    print(x)
-allSet = set()
-for x in dataList:
-    allSet |= set(x)
-print(len(allSet))
-for x in allSet:
-    print(x)
+phones = {'police' : 102, 'ambulance' : 103, 'firefighters' : 101}
+for service, phone in phones.items():
+    print(service, phone)
