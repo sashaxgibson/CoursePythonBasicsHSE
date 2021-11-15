5063cbf99bff10e5db7108bd137dbed79b5299b4
sashaxgibson <riseupgbs@gmail.com>
2021-11-15 14:06:34 +0300

    wip

--- TEST.py
+++
@@ -1,3 +0,0 @@
-a = ['A','a','a','B','b','C','c','d','E']
-print(sorted(a))
-print('A' < 'a')
--- W7_Samoe4astoeSlovo.py
+++ W7_Samoe4astoeSlovo.py
@@ -1,14 +1,11 @@
 inFile = open('input.txt')
 a = inFile.read().split()
-print(a)
-slavear = {}
+slavear = dict()
 for cur in a:
     if cur in slavear:
         slavear[cur] += 1
     else:
         slavear[cur] = 1
 print(slavear)
-print(*slavear.values())
 for i in slavear:
-    print(i)
-print(slavear.va)
+    print(i, slavear[i])
