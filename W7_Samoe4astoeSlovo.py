9e6f84f213ea03a05bf65edaaab3c4c772c1b9dd
sashaxgibson <riseupgbs@gmail.com>
2021-11-15 14:47:28 +0300

    Update W7_Samoe4astoeSlovo.py

--- W7_Samoe4astoeSlovo.py
+++ W7_Samoe4astoeSlovo.py
@@ -7,5 +7,9 @@ for cur in a:
     else:
         slavear[cur] = 1
 print(slavear)
+listWrd = []
 for i in slavear:
     print(i, slavear[i])
+    listWrd.append((i, slavear[i]))
+print(listWrd)
+print(listWrd[2][0])
