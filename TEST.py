2a47144103ee3d74314b489630f9c9fb47073301
sashaxgibson <riseupgbs@gmail.com>
2021-12-03 03:34:43 +0300

    wip

--- TEST.py
+++ TEST.py
@@ -1,6 +1,7 @@
-a = {'CHLEN': 'HUI','BENIS': 'PENIS', 'BAGINA': 'VAGINA', 'DUPA': 'POPA'}
-b = sorted(a)
-
-print(b)
-print(a)
-print(*a)
+dict1 = {1: 1, 2: 9, 3: 4}
+#sorted_values = sorted(dict1.values()) # Sort the values
+#print(sorted_values)
+#print()
+print(*dict1.values())
+sorted_keys = sorted(dict1, key=dict1.get)
+print(sorted_keys)
