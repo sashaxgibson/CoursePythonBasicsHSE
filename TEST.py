0200518abe2bfc6a523a358ad67e35c16825cc19
sashaxgibson <riseupgbs@gmail.com>
2022-08-28 15:06:37 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,1 +1,10 @@
-print(*filter(lambda x: x > 0, map(int, input().split())))
+print(
+    *filter(
+        lambda x: x > 0,
+        map(
+            int,
+                input().split()
+        )
+    )
+)
+
