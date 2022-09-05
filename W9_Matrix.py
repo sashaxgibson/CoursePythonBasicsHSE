bba0a90cf00aded9482cc1d39a020073b3156d87
sashaxgibson <riseupgbs@gmail.com>
2022-09-05 18:03:48 +0300

    Update W9_Matrix.py

--- W9_Matrix.py
+++ W9_Matrix.py
@@ -1,4 +1,13 @@
 from sys import stdin
 
 class Matrix:
-    def __init__(self, x, y):
+    penis
+
+m = Matrix([[1, 0], [0, 1]])
+print(m)
+m = Matrix([[2, 0, 0], [0, 1, 10000]])
+print(m)
+m = Matrix([[-10, 20, 50, 2443], [-5235, 12, 4324, 4234]])
+print(m)
+
+#exec(stdin.read())
