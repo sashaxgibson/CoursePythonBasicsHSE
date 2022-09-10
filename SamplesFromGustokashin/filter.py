be13bb0e8a48fcb50570a7bb2149ebb70366a4c4
sashaxgibson <riseupgbs@gmail.com>
2022-09-10 21:41:06 +0300

    unused files deleted

--- SamplesFromGustokashin/filter.py
+++
@@ -1,9 +0,0 @@
-print(
-    *filter(
-        lambda x: x > 0,
-        map(
-            int,
-            input().split()
-        )
-    )
-)
--- SamplesFromGustokashin/inertools_sampler.py
+++
@@ -1,3 +0,0 @@
-import itertools
-
-itertools.combinations()
