06da7323a96f3dcae837ab6dfc465fd783638393
sashaxgibson <riseupgbs@gmail.com>
2022-09-10 21:39:57 +0300

    deleted unused files

--- Week 2/test2.py
+++
@@ -1,8 +0,0 @@
-a, b, c = int(input()), int(input()), int(input())
-if not a <= b <= c:
-    (a, b) = (b, a)
-if not a <= b <= c:
-    (b, c) = (c, b)
-if not a <= b <= c:
-    (a, b) = (b, a)
-print(a, b, c)
