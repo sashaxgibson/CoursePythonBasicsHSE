1d32b629864e32721b3004e77933bf2fcc8b60e9
sashaxgibson <riseupgbs@gmail.com>
2022-09-14 06:19:44 +0300

    wip

---
+++ Test/Deepcopy.py
@@ -0,0 +1,19 @@
+from copy import deepcopy
+
+basedList = [[10, 10, 10], [11, 11, 11], [12, 12, 12]]
+cutList = basedList[:]
+copiedList = deepcopy(basedList)
+
+tstList = []
+for i in basedList:
+    tstList.append(i)
+
+
+basedList[0][1] = 88
+basedList[1][1] = 101
+basedList[2][1] = 999
+
+print('basedList', basedList)
+print('cutList', cutList)
+print('copiedList', copiedList)
+print('tstList', tstList)
--- W9_Matrix.py
+++ W9_Matrix.py
@@ -3,12 +3,3 @@ from sys import stdin
 class Matrix:
     def __init__(self, ListOfLists):
         self.obj_list = ListOfLists
-
-m = Matrix([[1, 0], [0, 1]])
-print(m)
-m = Matrix([[2, 0, 0], [0, 1, 10000]])
-print(m)
-m = Matrix([[-10, 20, 50, 2443], [-5235, 12, 4324, 4234]])
-print(m)
-
-#exec(stdin.read())
