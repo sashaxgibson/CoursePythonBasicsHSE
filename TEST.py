ae05b4878d94bf746fecaf705bfa32a50c2f8706
sashaxgibson <riseupgbs@gmail.com>
2022-09-15 01:52:04 +0300

    wip

--- TEST.py
+++ TEST.py
@@ -1,8 +1,2 @@
-class Dog:
-    def __init__(self):
-        self.color = "серый"
-
-
-bublik = Dog()
-print("Цвет этой собаки - ", bublik.color, "Это обычный цвет", "собакен просто немытый", sep='\t', end='\n')
-print("Zalupa Dlja Proverki")
+s = input()
+print(s.isalpha())
