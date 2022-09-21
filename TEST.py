642be22dbb5032aa5733e123a75b80dab9d852db
sashaxgibson <riseupgbs@gmail.com>
2022-09-21 05:52:23 +0300

    Update TEST.py

---
+++ TEST.py
@@ -0,0 +1,33 @@
+class Complex:
+    def __init__(self, re=0, im=0):
+        self.re = re
+        self.im = im
+
+    def __str__(self):
+        strRep = str(self.re)
+        if self.im >= 0:
+            strRep += '+'
+        strRep += str(self.im) + 'i'
+        return strRep
+
+    def __add__(self, other):
+        newRe = self.re + other.re
+        newIm = self.im + other.im
+        return Complex(newRe, newIm)
+
+    def __mul__(self, other):
+        if isinstance(other, Complex):
+            newRe = self.re * other.re - self.im * other.im
+            newIm = self.re * other.im + self.im * other.re
+        elif isinstance(other, int) or isinstance(other, float):
+            newRe = self.re * other
+            newIm = self.im * other
+        return Complex(newRe, newIm)
+
+    __rmul__ = __mul__
+
+
+a = Complex(1, 2)
+b = Complex(3, -4.5)
+print(a * b)
+print(a * 2)
