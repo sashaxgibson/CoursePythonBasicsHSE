4f9081a23e2bcae7acf19fc65801057d6393d1c4
sashaxgibson <riseupgbs@gmail.com>
2024-12-23 18:00:41 +0300

    Update W2_Chocolate.py

--- Week 2/W2_Chocolate.py
+++ Week 2/W2_Chocolate.py
@@ -1,1 +1,10 @@
-n, m, k = int(input()), int(input()), int(input())
+n = int(input())
+m = int(input())
+k = int(input())
+if k > n * m:
+    print("NO")
+else:
+    if ((n * m) - k) % n == 0 or ((n * m) - k) % m == 0:
+        print("YES")
+    else:
+        print("NO")
